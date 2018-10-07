a241562c2bdbeeb145f15082a8164b6df0315f8b
EsterGonzalezG <estergonzalezgalindo@gmail.com>
2018-10-07 13:47:08 +0200

    images

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,9 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <input className="input__search" placeholder="Filtra pokemons por nombre..." type="text" onChange={this.props.searchPokemon} />
-        <PokemonList  pokedex={this.props.pokedex} pokemonName={this.props.pokemonName} />
+        <input className="input__search" placeholder="Filtra pokemons por nombre..."
+          type="text" onChange={this.props.searchPokemon} />
+        <PokemonList pokedex={this.props.pokedex} pokemonName={this.props.pokemonName} />
       </div>
     );
   }
