68cb22798f5f7bda9fc494854dbae07ed042f37e
EsterGonzalezG <estergonzalezgalindo@gmail.com>
2018-10-07 14:29:44 +0200

    github pages

---
+++ docs/precache-manifest.7b065e923fd59493e696a51e2bfb6ec6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0d8ed367c571b4ea8c91",
+    "url": "./static/js/main.0d8ed367.chunk.js"
+  },
+  {
+    "revision": "35088d02d3ad8285d241",
+    "url": "./static/js/1.35088d02.chunk.js"
+  },
+  {
+    "revision": "0d8ed367c571b4ea8c91",
+    "url": "./static/css/main.d2feb65e.chunk.css"
+  },
+  {
+    "revision": "201a3015c01fefc20140632bbe8aaddb",
+    "url": "./index.html"
+  }
+];
